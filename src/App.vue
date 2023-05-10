0420b3b9f0e461eb55245e1dd2a82bf3743e17aa
ptrv-dev <petrpv99119@gmail.com>
2023-05-10 20:05:08 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<template>
+  <div class="max-w-screen-md mx-auto">
+    <h1 class="text-4xl font-bold">Hello World!</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi eos
+      fugit minus nemo voluptatibus fuga architecto quidem, eius quisquam
+      tempore odio consectetur unde deserunt quis ipsa sapiente nobis nam qui.
+    </p>
+  </div>
+</template>
