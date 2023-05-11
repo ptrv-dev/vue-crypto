28b5682e724498ea90d10ce4558381deb719c3ae
ptrv-dev <petrpv99119@gmail.com>
2023-05-11 10:28:27 +0300

    API validate token query function

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      tokenQuery: '',
+      tokens: [],
+    };
+  },
+  methods: {
+    addToken() {
+      if (!this.tokenQuery)
+        return alert('Please enter the token name or symbol');
+      this.tokens.push({
+        name: '',
+        symbol: this.tokenQuery.toUpperCase(),
+        price: null,
+      });
+      this.tokenQuery = '';
+    },
+  },
 };
 </script>
 
@@ -8,11 +26,14 @@ export default {
   <div class="container mt-4">
     <div class="inline-flex gap-2">
       <input
+        v-model="tokenQuery"
+        @keydown.enter="addToken"
         class="px-4 py-2 rounded-full border border-slate-700 outline-none ring-slate-200 focus:ring-4"
         type="text"
         placeholder="Enter token name..."
       />
       <button
+        @click="addToken"
         class="px-8 py-2 rounded-full text-white bg-slate-700 font-bold outline-none ring-slate-200 focus:ring-4"
       >
         Add
@@ -21,130 +42,29 @@ export default {
   </div>
   <hr class="container border-slate-200 my-4" />
   <div class="container">
-    <h1 class="text-2xl mb-4">Your watch list:</h1>
+    <h1 class="text-2xl mb-4">
+      {{
+        tokens.length
+          ? 'Your watch list:'
+          : 'Your watch list is empty right now...'
+      }}
+    </h1>
     <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
       <div
+        v-for="tokenData in tokens"
         class="flex flex-col border-2 border-dashed rounded-2xl border-slate-700 p-4 items-center gap-2"
       >
-        <p class="text-lg">BTC / USD</p>
-        <strong class="text-2xl">{{
-          (27891.93).toLocaleString('en-EN', {
-            style: 'currency',
-            currency: 'USD',
-          })
-        }}</strong>
-        <button
-          class="flex items-center gap-1 opacity-50 hover:opacity-100 outline-none ring-slate-200 focus:ring-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            class="w-5 h-5"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Remove
-        </button>
-      </div>
-      <div
-        class="flex flex-col border-2 border-dashed rounded-2xl border-slate-700 p-4 items-center gap-2"
-      >
-        <p class="text-lg">BTC / USD</p>
-        <strong class="text-2xl">{{
-          (27891.93).toLocaleString('en-EN', {
-            style: 'currency',
-            currency: 'USD',
-          })
-        }}</strong>
-        <button
-          class="flex items-center gap-1 opacity-50 hover:opacity-100 outline-none ring-slate-200 focus:ring-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            class="w-5 h-5"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Remove
-        </button>
-      </div>
-      <div
-        class="flex flex-col border-2 border-dashed rounded-2xl border-slate-700 p-4 items-center gap-2"
-      >
-        <p class="text-lg">BTC / USD</p>
-        <strong class="text-2xl">{{
-          (27891.93).toLocaleString('en-EN', {
-            style: 'currency',
-            currency: 'USD',
-          })
-        }}</strong>
-        <button
-          class="flex items-center gap-1 opacity-50 hover:opacity-100 outline-none ring-slate-200 focus:ring-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            class="w-5 h-5"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Remove
-        </button>
-      </div>
-      <div
-        class="flex flex-col border-2 border-dashed rounded-2xl border-slate-700 p-4 items-center gap-2"
-      >
-        <p class="text-lg">BTC / USD</p>
-        <strong class="text-2xl">{{
-          (27891.93).toLocaleString('en-EN', {
-            style: 'currency',
-            currency: 'USD',
-          })
-        }}</strong>
-        <button
-          class="flex items-center gap-1 opacity-50 hover:opacity-100 outline-none ring-slate-200 focus:ring-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            class="w-5 h-5"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Remove
-        </button>
-      </div>
-      <div
-        class="flex flex-col border-2 border-dashed rounded-2xl border-slate-700 p-4 items-center gap-2"
-      >
-        <p class="text-lg">BTC / USD</p>
-        <strong class="text-2xl">{{
-          (27891.93).toLocaleString('en-EN', {
-            style: 'currency',
-            currency: 'USD',
-          })
-        }}</strong>
+        <p class="text-lg">{{ tokenData.symbol }} / USD</p>
+        <strong class="text-2xl">
+          {{
+            tokenData.price
+              ? tokenData.price.toLocaleString('en-EN', {
+                  style: 'currency',
+                  currency: 'USD',
+                })
+              : '–'
+          }}
+        </strong>
         <button
           class="flex items-center gap-1 opacity-50 hover:opacity-100 outline-none ring-slate-200 focus:ring-2"
         >
---
+++ src/api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const validateTokenQuery = async (query) => {
+  try {
+    const { data } = await axios.get(
+      `https://api.coincap.io/v2/assets?search=${query}`
+    );
+    const tokensList = data.data;
+    const token = tokensList.find(
+      (t) =>
+        t.symbol.toLowerCase() === query.toLowerCase() ||
+        t.name.toLowerCase() === query.toLowerCase()
+    );
+    if (!token) return null;
+    return token;
+  } catch (error) {
+    return null;
+  }
+};
