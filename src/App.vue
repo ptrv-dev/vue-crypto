1745355e536e4ec1ea3824e857e0f45f46f5ce4e
ptrv-dev <petrpv99119@gmail.com>
2023-05-11 17:51:48 +0300

    AppToken component created and root component refactor

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script>
 import AppTickerAdd from './components/AppTickerAdd.vue';
+import AppToken from './components/AppToken.vue';
 import AppGraph from './components/AppGraph.vue';
 
 export default {
   name: 'App',
-  components: { AppGraph, AppTickerAdd },
+  components: { AppTickerAdd, AppToken, AppGraph },
   data() {
     return {
       tokenQuery: '',
@@ -98,34 +99,12 @@ export default {
       }}
     </h1>
     <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
-      <div
-        v-for="tokenData in tokens"
-        @click="selectedToken = tokenData"
-        class="flex flex-col border-2 border-dashed rounded-2xl border-slate-700 p-4 items-center gap-2 cursor-pointer"
-      >
-        <p class="text-lg">{{ tokenData.symbol }} / USD</p>
-        <strong class="text-2xl">
-          {{ tokenData.price ? `$${tokenData.price}` : '–' }}
-        </strong>
-        <button
-          @click.stop="removeToken(tokenData)"
-          class="flex items-center gap-1 opacity-50 hover:opacity-100 outline-none ring-slate-200 focus:ring-2"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            class="w-5 h-5"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Remove
-        </button>
-      </div>
+      <app-token
+        v-for="token in tokens"
+        :token="token"
+        @click="selectedToken = token"
+        @remove="removeToken(token)"
+      />
     </div>
   </div>
   <template v-if="selectedToken">
